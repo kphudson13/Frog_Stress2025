22fac0264dba744759aa0ec55d3ce66e074e3cc6
kphudson13 <kphudson@live.ca>
2026-08-20 23:59:44 -0600

    Plot automation updates

--- Analysis_Scripts/MasterScript.R
+++ Analysis_Scripts/MasterScript.R
@@ -15,121 +15,170 @@ library(rsq) # for partial r2
 rm(list=ls()) # Clear environment
 
 Data <- read.csv("Data_Spreadsheet.csv") %>%
-  mutate(VCO2 = ifelse(VCO2 <= 0, NA, VCO2)) %>%
-  mutate(mW = VCO2 * 21.1) %>% # convert vco2 to watts
-  filter(Notes != "High CV")
+  mutate(VCO2 = as.numeric(VCO2),
+         VCO2 = ifelse(VCO2 <= 0, NA, VCO2), # remove NAS
+         mW = VCO2 * 21.1 / 60 * 100) %>%
+  filter(Notes != "High CV") 
 
 dir.create("Figures", showWarnings = FALSE) # create directory for figures
 
-# VCO2 Models ------------------------------------------------------------------
+# Raw Models ------------------------------------------------------------------
 
-MSMRWeightTemp_Model <- lm(data = Data, log(mW/Weight) ~ log(Weight) + Temperature)
+MSMRWeightTemp_Unfiltered <- lm(data = Data, log(mW/Weight) ~ log(Weight) + Temperature)
+CortMSMR_Unfiltered <- lm(data = Data, log(Cort) ~ log(mW/Weight)) 
+CortWeightTemp_Unfiltered <- lm(data = Data, log(Cort) ~ log(Weight) + Temperature)
+
+# Cook's Distance Function ---------------------------------------------------
+
+CDist_fun <- function(mod, formula, data) {
+  
+  CD <- cooks.distance(mod)
+  keep <- CD <= 4 / nobs(mod) 
+  model_rows <- as.integer(names(residuals(mod)))
+  model_data <- data[model_rows, , drop = FALSE]
+  filtered_data <- model_data[keep, , drop = FALSE]
+  
+  model_name <- deparse(substitute(mod))
+  obj_name <- sub("_Unfiltered$", "", model_name)
+  data_name <- paste0(obj_name, "_Data")
+  
+  assign(data_name, filtered_data, envir = .GlobalEnv) # Save filtered data
+  
+  # do.call forces the data argument to resolve to the actual named global object
+  filtered_model <- do.call(lm,
+                            list(formula = formula, data = as.name(data_name)),
+                            envir = .GlobalEnv)
+  
+  assign(paste0(obj_name, "_Model"), filtered_model, envir = .GlobalEnv)
+  
+  invisible(filtered_model)
+}
+
+# Run Cook's distance filtering -----------------------------------------------
+
+CDist_fun(MSMRWeightTemp_Unfiltered,
+          log(mW/Weight) ~ log(Weight) + Temperature,
+          Data)
+
 summary(MSMRWeightTemp_Model)
-CortMSMR_Model <- lm(data = Data, log(Cort) ~ log(mW/Weight)) 
+
+CDist_fun(CortMSMR_Unfiltered,
+          log(Cort) ~ log(mW/Weight),
+          Data)
+
 summary(CortMSMR_Model)
-CortWeightTemp_Model <- lm(data = Data, log(Cort) ~ log(Weight) + Temperature)
+
+CDist_fun(CortWeightTemp_Unfiltered,
+          log(Cort) ~ log(Weight) + Temperature,
+          Data)
+
 summary(CortWeightTemp_Model)
 
 # Plots -------------------------------------------------------------------
 
 theme1 <- theme(legend.background = element_blank(),
                 legend.title = element_blank(),
-                legend.key.height = unit(0.6, "lines"),
-                legend.position=c(.8,0.1)) # set standard theme for plots
+                legend.key.height = unit(0.6, "lines"), # fit legend lines closer together
+                legend.position=c(.8, 0.1)) # set standard theme for plots
 
 # normalized points for plotting
-Data$MSMR_NormTemp <- log(Data$mW/Data$Weight) - MSMRWeightTemp_Model[["coefficients"]][["Temperature"]]*Data$Temperature
+MSMRWeightTemp_Data$MSMR_NormTemp <- log(MSMRWeightTemp_Data$mW/MSMRWeightTemp_Data$Weight) - MSMRWeightTemp_Model[["coefficients"]][["Temperature"]]*MSMRWeightTemp_Data$Temperature
 
-(MSMRWeight_Plot <- ggplot(Data, aes(x = log(Weight), y = MSMR_NormTemp)) +
+(MSMRWeight_Plot <- ggplot(MSMRWeightTemp_Data, aes(x = log(Weight), y = MSMR_NormTemp)) +
     geom_point(aes(colour = Species)) +
     geom_abline(intercept = coefficients(summary(MSMRWeightTemp_Model))[1,1],
                 slope = coefficients(summary(MSMRWeightTemp_Model))[2,1]) + # pull lines from model
     theme_classic() +
     theme1 +
-    annotate("text", size = 3.5, x = 1, y = -10,
+    annotate("text", size = 3.5, 
+             x = mean(log(MSMRWeightTemp_Data$Weight))*0.9, # formatted this way to the location is dynamic
+             y = mean(MSMRWeightTemp_Data$MSMR_NormTemp)*0.9,
              label = list(bquote(atop(y==~ .(round(coefficients(summary(MSMRWeightTemp_Model))[1,1], 2))
                                       ~x^.(round(coefficients(summary(MSMRWeightTemp_Model))[2,1], 2)),
                                       Partial ~R^2 ==~ .(round(rsq.partial(MSMRWeightTemp_Model)$partial.rsq[1], 2))))),
-             parse = TRUE) +
-    scale_x_continuous(limits = c(-1.5, 4)) +
-    scale_y_continuous(limits = c(-12, -6))
-)
+             parse = TRUE) 
+  # +
+  #   scale_x_continuous(limits = c(-1, 4)) +
+  #   scale_y_continuous(limits = c(-4, 8))
+) 
 
 ggsave(filename = "Figures/MSMRWeight_Plot.png", width=90, height=90, units="mm") #save a picture
 
 # normalized points for plotting
-Data$MSMR_NormWeight <- log(Data$mW/Data$Weight) - MSMRWeightTemp_Model[["coefficients"]][["log(Weight)"]]*log(Data$Weight)
+MSMRWeightTemp_Data$MSMR_NormWeight <- log(MSMRWeightTemp_Data$mW/MSMRWeightTemp_Data$Weight) - MSMRWeightTemp_Model[["coefficients"]][["log(Weight)"]]*log(MSMRWeightTemp_Data$Weight)
 
-(MSMRTemp_Plot <- ggplot(data = Data, aes(x = Temperature, y = MSMR_NormWeight)) +
+(MSMRTemp_Plot <- ggplot(MSMRWeightTemp_Data, aes(x = Temperature, y = MSMR_NormWeight)) +
     geom_point(aes(colour = Species)) +
     geom_abline(intercept = coefficients(summary(MSMRWeightTemp_Model))[1,1],
                 slope = coefficients(summary(MSMRWeightTemp_Model))[3,1]) +
     theme_classic() +
     theme1 +
-    annotate("text", size = 3.5, x = 22, y = -10,
+    annotate("text", size = 3.5, 
+             x = mean(MSMRWeightTemp_Data$Temperature)*0.8, # formatted this way to the location is dynamic
+             y = mean(MSMRWeightTemp_Data$MSMR_NormWeight)*0.8,
              label = list(bquote(atop(y==~ .(round(coefficients(summary(MSMRWeightTemp_Model))[1,1], 2))
                                       ~x^.(round(coefficients(summary(MSMRWeightTemp_Model))[3,1], 2)),
                                       Partial ~R^2 ==~ .(round(rsq.partial(MSMRWeightTemp_Model)$partial.rsq[2], 2))))),
-             parse = TRUE)+
-    scale_x_continuous(limits = c(12, 36)) +
-    scale_y_continuous(limits = c(-12, -6))
+             parse = TRUE) 
 )
 
 ggsave(filename = "Figures/MSMRTemp_Plot.png", width=90, height=90, units="mm") #save a picture
 
-(CortMSMR_Plot <- ggplot(data = Data, aes(x=log(mW/Weight), y = log(Cort))) +
+(CortMSMR_Plot <- ggplot(CortMSMR_Data, aes(x=log(mW/Weight), y = log(Cort))) +
     geom_point(aes(colour = Species)) +
     geom_abline(intercept = coefficients(summary(CortMSMR_Model))[1,1],
                 slope = coefficients(summary(CortMSMR_Model))[2,1]) + 
     theme_classic() +
     theme1 +
-    annotate("text", size = 3.5, x = -10, y = 6,
+    annotate("text", size = 3.5, 
+             x = mean(log(CortMSMR_Data$mW/CortMSMR_Data$Weight))*0.9, 
+             y = mean(log(CortMSMR_Data$Cort))*1.2,
              label = list(bquote(atop(y==~ .(round(coefficients(summary(CortMSMR_Model))[1,1], 2))
                                       ~x^.(round(coefficients(summary(CortMSMR_Model))[2,1], 2)),
                                       ~R^2 ==~ .(round(summary(CortMSMR_Model)$r.squared, 2))))),
-             parse = TRUE) +
-    scale_x_continuous(limits = c(-11, -6)) +
-    scale_y_continuous(limits = c(-4, 8))
+             parse = TRUE) #+
+  # scale_x_continuous(limits = c(-11, -6)) +
+  # scale_y_continuous(limits = c(-4, 8))
 )
 
 ggsave(filename = "Figures/CortMSMR_Plot.png", width=90, height=90, units="mm") #save a picture
 
 # normalized points for plotting
-Data$Cort_NormTemp <- log(Data$Cort) - CortWeightTemp_Model[["coefficients"]][["Temperature"]]*Data$Temperature
+CortWeightTemp_Data$Cort_NormTemp <- log(CortWeightTemp_Data$Cort) - CortWeightTemp_Model[["coefficients"]][["Temperature"]]*CortWeightTemp_Data$Temperature
 
-(CortWeight_Plot <- ggplot(data = Data, aes(x=log(Weight), y = Cort_NormTemp)) +
+(CortWeight_Plot <- ggplot(CortWeightTemp_Data, aes(x=log(Weight), y = Cort_NormTemp)) +
     geom_point(aes(colour = Species)) +
     geom_abline(intercept = coefficients(summary(CortWeightTemp_Model))[1,1],
                 slope = coefficients(summary(CortWeightTemp_Model))[2,1]) +
     theme_classic() +
     theme1 +
-    annotate("text", size = 3.5, x = 3, y = 5,
-             label = list(bquote(atop(y==~ .(round(coefficients(summary(CortWeightTemp_Model))[1,1], 2))
-                                      ~x^.(round(coefficients(summary(CortWeightTemp_Model))[2,1], 2)),
-                                      Partial ~R^2 ==~ .(round(rsq.partial(CortWeightTemp_Model)$partial.rsq[1], 2))))),
-             parse = TRUE) +
-    scale_x_continuous(limits = c(-1, 4)) +
-    scale_y_continuous(limits = c(-2, 6))
+  annotate("text", size = 3.5, x = 3, y = 3,
+           label = list(bquote(atop(y==~ .(round(coefficients(summary(CortWeightTemp_Model))[1,1], 2))
+                                    ~x^.(round(coefficients(summary(CortWeightTemp_Model))[2,1], 2)),
+                                    Partial ~R^2 ==~ .(round(rsq.partial(CortWeightTemp_Model)$partial.rsq[1], 2))))),
+           parse = TRUE) #+
+  # scale_x_continuous(limits = c(-1, 4)) +
+  # scale_y_continuous(limits = c(-2, 6))
 )
 
 ggsave(filename = "Figures/CortWeight_Plot.png", width=90, height=90, units="mm") #save a picture
 
 # normalized points for plotting
-Data$Cort_NormWeight <- log(Data$Cort) - CortWeightTemp_Model[["coefficients"]][["log(Weight)"]]*log(Data$Weight)
+CortWeightTemp_Data$Cort_NormWeight <- log(CortWeightTemp_Data$Cort) - CortWeightTemp_Model[["coefficients"]][["log(Weight)"]]*log(CortWeightTemp_Data$Weight)
 
-(CortTemp_Plot <- ggplot(data = Data, aes(x=Temperature, y = Cort_NormWeight)) +
+(CortTemp_Plot <- ggplot(CortWeightTemp_Data, aes(x=Temperature, y = Cort_NormWeight)) +
     geom_point(aes(colour = Species)) +
     geom_abline(intercept = coefficients(summary(CortWeightTemp_Model))[1,1],
                 slope = coefficients(summary(CortWeightTemp_Model))[3,1]) +
     theme_classic() +
     theme1 +
-    annotate("text", size = 3.5, x = 23, y = 5,
-             label = list(bquote(atop(y==~ .(round(coefficients(summary(CortWeightTemp_Model))[1,1], 2))
-                                      ~x^.(round(coefficients(summary(CortWeightTemp_Model))[3,1], 2)),
-                                      Partial ~R^2 ==~ .(round(rsq.partial(CortWeightTemp_Model)$partial.rsq[2], 2))))),
-             parse = TRUE) +
-    scale_x_continuous(limits = c(12, 36)) +
-    scale_y_continuous(limits = c(-2, 6))
+  annotate("text", size = 3.5, x = 23, y = 3,
+           label = list(bquote(atop(y==~ .(round(coefficients(summary(CortWeightTemp_Model))[1,1], 2))
+                                    ~x^.(round(coefficients(summary(CortWeightTemp_Model))[3,1], 2)),
+                                    Partial ~R^2 ==~ .(round(rsq.partial(CortWeightTemp_Model)$partial.rsq[2], 2))))),
+           parse = TRUE) #+
+  # scale_x_continuous(limits = c(12, 36)) +
+  # scale_y_continuous(limits = c(-2, 6))
 )
 
 ggsave(filename = "Figures/CortTemp_Plot.png", width=90, height=90, units="mm") #save a picture
