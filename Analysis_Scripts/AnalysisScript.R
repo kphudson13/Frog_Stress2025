ee76f8e6df41375667ace79f97d6ed124554eb6c
kphudson13 <kphudson@live.ca>
2026-09-01 11:54:03 -0600

    Rename for organization

--- Analysis_Scripts/AnalysisScript.R
+++ Analysis_Scripts/AnalysisScript.R
@@ -7,27 +7,12 @@
 # Live laugh love
 # =========================================================
 
-library(tidyverse)
-library(grid)
-library(gridExtra) # for stats table theme
-library(rsq) # for partial r2
-
-rm(list=ls()) # Clear environment
-
-Data <- read.csv("Data_Spreadsheet.csv") %>%
-  mutate(VCO2 = ifelse(VCO2 <= 0, NA, VCO2),
-         mW = VCO2 * 21.1 / 60 * 1000, # energetic conversion(jouls per ml), min to sec, and Watts to mW
-         Reproductive = ifelse(Reproductive == "Gravid", Reproductive, "Other"),
-         Sex = ifelse(is.na(Sex) | Sex == "", "Other", Sex),
-         MSMR = mW/Weight) 
-
-dir.create("Figures", showWarnings = FALSE) # create directory for figures
 
 # Raw Models ------------------------------------------------------------------
 
-MSMRWeightTemp_Unfiltered <- lm(data = Data, log(mW/Weight) ~ log(Weight) + Temperature + Species) 
+MSMRWeightTemp_Unfiltered <- lm(data = Data, log(MSMR) ~ log(Weight) + Temperature + Species) 
 CortMSMR_Unfiltered <- lm(data = Data %>%
-                            filter(Notes != "High CV" ), log(Cort) ~ log(mW/Weight) + Species) # filter out high CV cort points 
+                            filter(Notes != "High CV" ), log(Cort) ~ log(MSMR) + Species) # filter out high CV cort points 
 CortWeightTemp_Unfiltered <- lm(data = Data %>%
                                   filter(Notes != "High CV" ), log(Cort) ~ log(Weight) + Temperature + Species)
 # & Notes != "Single well failure"
@@ -58,7 +43,7 @@ CDist_fun <- function(mod, formula, data) {
 # Run Cook's distance filtering -----------------------------------------------
 
 CDist_fun(MSMRWeightTemp_Unfiltered,
-          log(mW/Weight) ~ log(Weight) + Temperature + Species,
+          log(MSMR) ~ log(Weight) + Temperature + Species,
           Data)
 
 summary(MSMRWeightTemp_Model)
@@ -70,7 +55,7 @@ CDist_fun(CortWeightTemp_Unfiltered,
 summary(CortWeightTemp_Model)
 
 CDist_fun(CortMSMR_Unfiltered,
-          log(Cort) ~ log(mW/Weight) + Species,
+          log(Cort) ~ log(MSMR) + Species,
           Data)
 
 summary(CortMSMR_Model)
@@ -83,7 +68,7 @@ theme1 <- theme(legend.background = element_blank(), # make clear
                 legend.position=c(.8, 0.1)) # set standard theme for plots
 
 # normalized points for plotting
-MSMRWeightTemp_Data$MSMR_NormTemp <- log(MSMRWeightTemp_Data$mW/MSMRWeightTemp_Data$Weight) - MSMRWeightTemp_Model[["coefficients"]][["Temperature"]]*MSMRWeightTemp_Data$Temperature
+MSMRWeightTemp_Data$MSMR_NormTemp <- log(MSMRWeightTemp_Data$MSMR) - MSMRWeightTemp_Model[["coefficients"]][["Temperature"]]*MSMRWeightTemp_Data$Temperature
 
 (MSMRWeight_Plot <- ggplot(MSMRWeightTemp_Data, aes(x = log(Weight), y = MSMR_NormTemp)) +
     geom_point(aes(colour = Species)) +
@@ -93,7 +78,7 @@ MSMRWeightTemp_Data$MSMR_NormTemp <- log(MSMRWeightTemp_Data$mW/MSMRWeightTemp_D
     theme1 +
     annotate("text", size = 3.5, 
              x = mean(log(MSMRWeightTemp_Data$Weight))*0.9, # formatted this way to the location is dynamic
-             y = mean(MSMRWeightTemp_Data$MSMR_NormTemp)*0.6,
+             y = mean(MSMRWeightTemp_Data$MSMR_NormTemp)*0.5,
              label = list(bquote(atop(y==~ .(round(coefficients(summary(MSMRWeightTemp_Model))[1,1], 2))
                                       ~x^.(round(coefficients(summary(MSMRWeightTemp_Model))[2,1], 2)),
                                       Partial ~R^2 ==~ .(round(rsq.partial(MSMRWeightTemp_Model)$partial.rsq[1], 2))))),
@@ -101,13 +86,14 @@ MSMRWeightTemp_Data$MSMR_NormTemp <- log(MSMRWeightTemp_Data$mW/MSMRWeightTemp_D
     labs(x = "Weight(ln(g))",
          y = "Normalized MSMR (ln(mW/g))") +
     scale_x_continuous(limits = c(-1, 4)) +
-    scale_y_continuous(limits = c(-7.5, -2.5))
+    scale_y_continuous(limits = c(-5, 0))
 ) 
 
-ggsave(filename = "Figures/MSMRWeight_Plot.png", width=90, height=90, units="mm") #save a picture
+ggsave(filename = paste("Figures/", directory, "/MSMRWeight_Plot.png.png",sep = ""), 
+       width=90, height=90, units="mm") #save a picture
 
 # normalized points for plotting
-MSMRWeightTemp_Data$MSMR_NormWeight <- log(MSMRWeightTemp_Data$mW/MSMRWeightTemp_Data$Weight) - MSMRWeightTemp_Model[["coefficients"]][["log(Weight)"]]*log(MSMRWeightTemp_Data$Weight)
+MSMRWeightTemp_Data$MSMR_NormWeight <- log(MSMRWeightTemp_Data$MSMR) - MSMRWeightTemp_Model[["coefficients"]][["log(Weight)"]]*log(MSMRWeightTemp_Data$Weight)
 
 (MSMRTemp_Plot <- ggplot(MSMRWeightTemp_Data, aes(x = Temperature, y = MSMR_NormWeight)) +
     geom_point(aes(colour = Species)) +
@@ -125,7 +111,7 @@ MSMRWeightTemp_Data$MSMR_NormWeight <- log(MSMRWeightTemp_Data$mW/MSMRWeightTemp
     labs(x = "Temperature (c)",
          y = "Normalized MSMR (ln(mW/g))") +
     scale_x_continuous(limits = c(12, 40)) +
-    scale_y_continuous(limits = c(-6, 0))
+    scale_y_continuous(limits = c(-4, 0))
 )
 
 ggsave(filename = "Figures/MSMRTemp_Plot.png", width=90, height=90, units="mm") #save a picture
@@ -174,7 +160,7 @@ CortWeightTemp_Data$Cort_NormWeight <- log(CortWeightTemp_Data$Cort) - CortWeigh
 
 ggsave(filename = "Figures/CortTemp_Plot.png", width=90, height=90, units="mm") #save a picture
 
-(CortMSMR_Plot <- ggplot(CortMSMR_Data, aes(x=log(mW/Weight), y = log(Cort))) +
+(CortMSMR_Plot <- ggplot(CortMSMR_Data, aes(x=log(MSMR), y = log(Cort))) +
     geom_point(aes(colour = Species)) +
     geom_abline(intercept = coefficients(summary(CortMSMR_Model))[1,1],
                 slope = coefficients(summary(CortMSMR_Model))[2,1]) + 
@@ -189,42 +175,42 @@ ggsave(filename = "Figures/CortTemp_Plot.png", width=90, height=90, units="mm") 
              parse = TRUE) +
     labs(x = "MSMR (ln(mW/g))",
          y = "Cort (ln(ng/ml))") +
-    scale_x_continuous(limits = c(-7, -0)) +
+    scale_x_continuous(limits = c(-4, -0)) +
     scale_y_continuous(limits = c(-1, 7))
 )
 
 ggsave(filename = "Figures/CortMSMR_Plot.png", width=90, height=90, units="mm") #save a picture
 
-# Stats Table -------------------------------------------------------------
-
-
-Stats_Tab <- rbind(coefficients(summary(MSMRWeightTemp_Model)),
-                   coefficients(summary(CortWeightTemp_Model)),
-                   coefficients(summary(CortMSMR_Model))) %>%
-  as.data.frame(.) %>%
-  slice(-c(1,4,5,6,7,10,11)) %>%  #cut out all the rows of intercept stats
-  select(., -"t value") %>%
-  cbind(., c(rsq.partial(MSMRWeightTemp_Model)$partial.rsq[1:2], 
-             rsq.partial(CortWeightTemp_Model)$partial.rsq[1:2], 
-             summary(CortMSMR_Model)$r.squared)) %>% # bind in r sq
-  mutate(across(c(1:4), \(x) round(x, digits = 2))) %>% #new way to round w/ anonymous function
-  `colnames<-`(c("Estimate", "SE (Slope)", "p value", "R2")) %>%
-  `rownames<-`(c("MSMR ~ Weight", "MSMR ~ Temp", "Cort ~ Weight", "Cort ~ Temp", "Cort ~ MSMR"))
-
-tt1 <- ttheme_default(rowhead=list(fg_params=list(fontface = "bold"),
-                                   bg_params=list(fill="grey80"))) # theme for stats table
-
-write.csv(Stats_Tab, file = "Figures/StatsTab.csv", row.names = TRUE)
-
-#export stats table 
-png("Figures/StatsTab.png",
-    height = 180*nrow(Stats_Tab), 
-    width = 500*ncol(Stats_Tab),
-    res = 300)
-grid.newpage()
-grid.table(Stats_Tab, theme = tt1)
-grid.text("Stats Table", x = 0.2, y = 0.9, gp = gpar(fontface = "bold"))
-dev.off()
-
-
-
+# # Stats Table -------------------------------------------------------------
+# 
+# 
+# Stats_Tab <- rbind(coefficients(summary(MSMRWeightTemp_Model)),
+#                    coefficients(summary(CortWeightTemp_Model)),
+#                    coefficients(summary(CortMSMR_Model))) %>%
+#   as.data.frame(.) %>%
+#   slice(-c(1,4,5,6,7,10,11)) %>%  #cut out all the rows of intercept stats
+#   select(., -"t value") %>%
+#   cbind(., c(rsq.partial(MSMRWeightTemp_Model)$partial.rsq[1:2], 
+#              rsq.partial(CortWeightTemp_Model)$partial.rsq[1:2], 
+#              summary(CortMSMR_Model)$r.squared)) %>% # bind in r sq
+#   mutate(across(c(1:4), \(x) round(x, digits = 2))) %>% #new way to round w/ anonymous function
+#   `colnames<-`(c("Estimate", "SE (Slope)", "p value", "R2")) %>%
+#   `rownames<-`(c("MSMR ~ Weight", "MSMR ~ Temp", "Cort ~ Weight", "Cort ~ Temp", "Cort ~ MSMR"))
+# 
+# tt1 <- ttheme_default(rowhead=list(fg_params=list(fontface = "bold"),
+#                                    bg_params=list(fill="grey80"))) # theme for stats table
+# 
+# write.csv(Stats_Tab, file = "Figures/StatsTab.csv", row.names = TRUE)
+# 
+# #export stats table 
+# png("Figures/StatsTab.png",
+#     height = 180*nrow(Stats_Tab), 
+#     width = 500*ncol(Stats_Tab),
+#     res = 300)
+# grid.newpage()
+# grid.table(Stats_Tab, theme = tt1)
+# grid.text("Stats Table", x = 0.2, y = 0.9, gp = gpar(fontface = "bold"))
+# dev.off()
+# 
+# 
+# 
