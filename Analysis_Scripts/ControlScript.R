76580de60d624cb6e2f1c0b57acb10a663d8a948
kphudson13 <kphudson@live.ca>
2026-09-03 18:17:56 -0600

    Modify MSMR units

--- Analysis_Scripts/ControlScript.R
+++ Analysis_Scripts/ControlScript.R
@@ -19,9 +19,9 @@ source("Analysis_Scripts/CooksDistFunction.R")
 Whole_data <- read.csv("Data_Spreadsheet.csv") %>%
   mutate(VCO2 = ifelse(VCO2 <= 0, NA, VCO2),
          mW = VCO2 * 21.1 / 60 * 1000, # energetic conversion(jouls per ml), min to sec, and Watts to mW
+         MSMR = VCO2/Weight,
          Reproductive = ifelse(Reproductive == "Gravid", Reproductive, "Other"),
-         Sex = ifelse(is.na(Sex) | Sex == "", "Other", Sex),
-         MSMR = mW/Weight) 
+         Sex = ifelse(is.na(Sex) | Sex == "", "Other", Sex))
 
 # All Data ----------------------------------------------------------------
 
@@ -44,7 +44,7 @@ source("Analysis_Scripts/AnalysisScript.R")
 # No high CV or single well failure ---------------------------------------
 
 Data = Whole_data %>%
-  filter(Notes != "High CV" & Notes != "Single well failiure")
+  filter(Notes != "High CV" & Notes != "Single well failure")
 
 directory <- "FilterCVSWF" 
 CreateDR(directory)
